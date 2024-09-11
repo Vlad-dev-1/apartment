eb5d8206ba8585f31a0e5be89e4f2b07d5f0f6f8
Bakulin Vladimir <wal1984@mail.ru>
2024-09-11 18:38:01 +0700

    [RAA-002] Добавление модуля Server

--- architect_module/src/main/java/com/example/architect/config/EmptyMigrationStrategyConfig.java
+++ architect_module/src/main/java/com/example/architect/config/EmptyMigrationStrategyConfig.java
@@ -7,6 +7,8 @@ import org.springframework.context.annotation.Configuration;
 @Configuration
 public class EmptyMigrationStrategyConfig {
 
+
+
     @Bean
     public FlywayMigrationStrategy flywayMigrationStrategy() {
         return flyway -> {
---
+++ server_module/src/main/java/com/example/server_module/ServerModuleApplication.java
@@ -0,0 +1,13 @@
+package com.example.server_module;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ServerModuleApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ServerModuleApplication.class, args);
+    }
+
+}
